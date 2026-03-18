e445597d3183f5d49216d922c4fbb6d45000c2ac
MELISSA MURRO RIBEIRO <melissa.ribeiro13@senaisp.edu.br>
2026-03-18 15:07:02 -0300

    feat(ex01): cadastro do aluno.

---
+++ src/ex01.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class ex01 {
+    static void main() {
+        Scanner sc = new Scanner(System.in);
+
+        IO.println("nome do aluno");
+        String nome = sc.next();
+
+        IO.println("curso matriculado");
+        String curso = sc.next();
+
+        IO.println("idade");
+        int idade = sc.nextInt();
+
+        IO.println("nome do aluno: " + nome);
+        IO.println("curso matriculado: " + curso);
+        IO.println("idade: " + idade);
+
+
+
+
+
+
+
+
+}
